240fefa2816ca74d7fa015add00622b72f20f945
Pilougame <pilouhenry@icloud.com>
2025-10-12 17:00:09 +0000

    bug found

--- usefool.js
+++ usefool.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var chalk = require("chalk");
 class f{
     static log(value) {
-        fs.appendFileSync("logs.txt", chalk.gray("LOG =>    ")+value+"\n");
+        fs.appendFileSync(path.join(os.homedir(), "logs.txt"), chalk.gray("LOG =>    ")+value+"\n");
         console.log(value);
     };
 };
